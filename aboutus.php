45e4de3bc54b38162ac712aeb27270ad900a4187
phucnt2166 <nphucno001@gmail.com>
2023-04-03 09:18:08 +0700

    about

---
+++ aboutus.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleHomepage.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- link để tải icon -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <div>
+        <h1>About us</h1>
+        <br>
+    </div>
+    <div class="paragraph" style="text-align:left;">
+        <font size="3">
+            "VENUS Handicraft is a well-known certified company. Established is  1972, VENUS Handicraft
+            is the field of manufacturing and exporting handicrafts since more than 36 years.
+            The products are of elegance, style and superior quality.
+            Showcasing a range, that is ethnic as well as innovative, the company has
+            established itself as a leading name is the field of handicrafts.
+            VENUS Handicrafts is acknowledged for its professional aproach in creativity,
+            design and exclusive products. The company understands the importance of 
+            Quality and Timely Delivery of merchandise in the competetive and challenging 
+            International Business scenario. Its dedicated team of employee & workers is
+            par excellence, skilled and are part of executing the orders with  excellent results in 
+            quallity, production and shipping.  "
+        </font>
+        <br>
+    </div>
+    <div class="paragraph" style="text-align:left;">
+        <font size="3">
+        The company manufactures a wide range of products which are made by
+        skilled Craftsmen. Artisans work hard to produce classic pieces of handicrafts
+        as well as.
+        With the wide experience of more than 36 years - the products are
+        distinguished in the market for their superior quality and elegant design.
+        </font>
+    </div>
+</body>
+</html>
