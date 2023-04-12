dc6211d4f270eb5ae554251f6962eece296fe9de
buithihoi <hoi.bt.2156@aptechlearning.edu.vn>
2023-04-13 00:16:37 +0700

    .

--- giao-dien/product_brass.php
+++ giao-dien/product_brass.php
@@ -2,10 +2,26 @@
 include 'header.php';
 include 'connect_db.php';
 ?>
-<br><br><br>
-<div class="container-fluid p-5 text-success text-center ">
-    <h2 style="Text-decoration : none;">Brass Product</h2><br>
-</div>
+<style>
+    .product-theme5 {
+        background: url(Images/theme3.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        width: 100%;
+        height: 350px;
+        color: white;
+        padding-top: 150px;
+        text-align: center;
+        text-decoration: none;
+    }
+</style>
+
+<h1 class="product-theme5">PRODUCT BRASS</h1>
+<!-- <div class="container" style="text-align:center;border: 5px solid grey;width:fit-content; "> -->
+    <!-- <h2> Brass products</h2> -->
+<!-- </div> -->
     <ul style="list-style-type: none;margin: 0;padding: 0;overflow: hidden;background-color: #fff;">
 
         <?php
@@ -21,7 +37,7 @@ include 'connect_db.php';
                     href="product_detail.php?Id=<?= $row['Id']; ?>">
 
                     <img src='image_DTB/<?= $row["Image"]; ?>' class='img-responsive'
-                        style='width:430px ; height :300px; border: 5px solid orange;' alt='Image'><br><br>
+                        style='width:350px ; height :350px; border: 5px solid orange;' alt='Image'><br><br>
 
                     <h4>
                         <?php echo $row["Title"]; ?>
